cf15bbe56f68c985e8c8190a77bc38dc1aff5a8f
Atrahasis <coffre.a@gmail.com>
2020-04-21 00:54:19 +0200

    Add upload script.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-    entry: "./index.ts",
+    entry: "./src/index.ts",
     output: { filename: "app.min.js" },
     resolve: {
         extensions: [".ts", ".js"],
     },
+    mode: "development",
     devtool: "source-map",
+    devServer: { publicPath: "/dist/" },
     module: {
         rules: [{ test: /\.ts$/, loader: "ts-loader" }],
     },
